f8fa20b4132efc6b5a2501ec1f7a4a5989a827ba
nanb <1605430072@qq.com>
2022-10-28 21:19:50 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author wuxy
+ * @create 2022-10-28 21:09
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
